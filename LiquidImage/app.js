5ef42d18cc8d453eb56eca3f70c3b365dd623d24
Laura C Paulino <40530704+lauxpaux@users.noreply.github.com>
2020-07-23 20:09:10 -0400

    first commit
    first commit

---
+++ LiquidImage/app.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'), 
+    intensity: 0.2, 
+    image1: './assets/Image1.jpg',
+    image2: './assets/Image2.jpg', 
+    speedIn: 2, 
+    speedOut: 2,
+    angle: Math.PI / 4,
+    displacementImage: './assets/heightMap.png'
+})
